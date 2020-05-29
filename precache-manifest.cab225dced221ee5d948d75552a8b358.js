d4701625058f498df68413edff812f311d0ad4b1
Chris Cosentino <chriscosentino@hotmail.com>
2020-05-29 00:01:18 -0400

    Updates

---
+++ precache-manifest.cab225dced221ee5d948d75552a8b358.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "099d9f5ef857c1abd041092db695d467",
+    "url": "/redux-todo/index.html"
+  },
+  {
+    "revision": "c04cc0c1781fa148618c",
+    "url": "/redux-todo/static/js/2.e92200a8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-todo/static/js/2.e92200a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f6677d2bb9267bb0aa6",
+    "url": "/redux-todo/static/js/main.8aceb192.chunk.js"
+  },
+  {
+    "revision": "c86fe03890b352f03939",
+    "url": "/redux-todo/static/js/runtime-main.00543ac5.js"
+  }
+]);
